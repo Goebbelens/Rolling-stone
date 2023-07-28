f8485932673d7f730923eee6b383290d7884115e
Goebbelens <doronin_ivan@mail.ru>
2023-07-28 15:13:00 +0300

    Add controls, camera, rolling stone
    Accidentally made rigidbody orbitting

---
+++ Assets/Scripts/StoneRoll.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneRoll : MonoBehaviour
+{
+    [Header ("References")]
+    private Rigidbody stone;
+    public Transform Pointer;
+    public Transform player;
+
+    [Header ("Movement")]
+    public float stoneSpeed = 1f;
+    public float stoneTurnSpeed = 1f;
+    public float rotationSpeedChange = 1f;
+    private float PrevDistance;
+    void Start()
+    {
+        stone = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.TryGetComponent<CharacterController>(out CharacterController player))
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0;
+
+        /*if (Mathf.Abs(direction.normalized.x) > Mathf.Abs(stone.velocity.normalized.x))
+        {
+            Vector3 xVector = Vector3.zero;
+            xVector.x = direction.normalized.x;
+            stone.AddForce(xVector * stoneTurnSpeed);
+        }
+        if (Mathf.Abs(direction.normalized.z) > Mathf.Abs(stone.velocity.normalized.z))
+        {
+            Vector3 zVector = Vector3.zero;
+            zVector.z = direction.normalized.z;
+            stone.AddForce(zVector * stoneTurnSpeed);
+        }*/
+        
+        if (Vector3.Distance(transform.position, player.position) > PrevDistance)
+        {
+            //stone.AddForce(direction.normalized * stoneSpeed * 3);
+        }
+        stone.AddForce(direction.normalized * stoneSpeed);
+        stone.velocity = (stone.velocity.normalized / 4) * stoneSpeed;
+        //Debug.Log("RigidBody Magnitude: " + stone.velocity.magnitude);
+        Debug.Log("Direction: " + direction);
+        /*Debug.Log("Direction norm" + direction.normalized + "\nDirection: " + direction);
+        Debug.Log("Velocity: " + stone.velocity);*/
+        Debug.Log("VelMagnitude: " + stone.velocity.magnitude);
+        Debug.Log("Distance: " + Vector3.Distance(transform.position, player.position) + "\nPrevDistance: " + PrevDistance);
+        PrevDistance = Vector3.Distance(transform.position, player.position);
+    }
+
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private CharacterController controller;
+    [SerializeField]
+    private Transform cam;
+
+    [Header("Movement, Gravity and Jump")]
+    public float speed = 6f;
+    public float jumpHeight = 3f;
+    public float fallSpeed = 1f;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 1f;
+
+    private float gravity = -9.81f;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    [Header("Character rotation align to camera")]
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+        
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+
+        if (direction.magnitude > 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime * fallSpeed;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
